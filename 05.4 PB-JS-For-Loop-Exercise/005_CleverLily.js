131d0f029fad51f6b2c71f64b08951a93bad3f91
Simeon Atanasov <sim.atanasov@gmail.com>
2024-10-31 07:56:21 +0200

    Update task 005
    Update task 005

---
+++ 05.4 PB-JS-For-Loop-Exercise/005_CleverLily.js
@@ -0,0 +1,38 @@
+function cleverLily(liliAge, washingMachine, singleToyPrice){
+
+    liliAge = Number(liliAge);
+    washingMachine = Number(washingMachine);
+    singleToyPrice = Number(singleToyPrice);
+    let toysCount = 0;
+    let savedMoney = 0;
+    let lastMoney = 0;
+
+    for(let year = 1; year <= liliAge; year++){
+
+        if(year % 2 !== 0){ гт6
+            toysCount++;
+        }
+        else {
+            savedMoney += (lastMoney + 9);
+            lastMoney += 10;
+        }
+    }
+
+    let totalSavedMoneyFromToys = toysCount * singleToyPrice;
+    let TotalSavedMoney = savedMoney + totalSavedMoneyFromToys;
+
+    if(TotalSavedMoney >= washingMachine){
+        let leftMoney = (TotalSavedMoney - washingMachine).toFixed(2);
+        console.log(`Yes! ${leftMoney}`);
+    }
+    else{
+        let neededMoney = (washingMachine - TotalSavedMoney).toFixed(2);
+        console.log(`No! ${neededMoney}`);        
+    }
+}
+
+cleverLily(
+    "10",
+    "170.00",
+    "6"
+);
