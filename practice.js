2c5b6c03a1ab37b4c7174fc977860a7fe407ea0b
Simeon Atanasov <sim.atanasov@gmail.com>
2025-01-23 06:00:25 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,6 +1,9 @@
-function petShop(a, b) {
-    let amount = ((a * 2.5) + (b * 4));
-    console.log(`${amount.toFixed(2)} lv.`)
+function yardGreening(a) {
+    let price = Number((a * 7.61).toFixed(2));
+    let discount = Number((0.18 * price).toFixed(2));
+    let finalPrice = price - discount;
+    console.log(`The final price is: ${finalPrice.toFixed(2)} lv.`);
+    console.log(`The discount is: ${discount.toFixed(2)} lv.`)
 }
 
-petShop(5, 4);
+yardGreening(540);
