4ec2657c72d32e2e7f471eca966b15bb0a124f48
Simeon Atanasov <sim.atanasov@gmail.com>
2025-01-27 07:49:15 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,9 +1,11 @@
-function fishTank(a, b, c, d) {
-    let  fishTankTotal = a * b * c;
-    let litersTotal = fishTankTotal * 0.001;
-    let  percentageFull = d * 0.01;
-    let liters = litersTotal*(1-percentageFull);
-    console.log(`${liters.toFixed(3)}`)
+function USDtoBGN(usd) {
+    if (isNaN(usd)) {
+        console.log("Please enter a valid Number.");
+        return;
+    }
+    let bgn = Number(usd) * 1.79549;
+    console.log(bgn.toFixed(2));
 }
 
-fishTank(85, 75, 47, 17);
+USDtoBGN("22");
+USDtoBGN("invalid");
