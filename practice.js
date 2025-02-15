dc822a4ba40662529f949b16858062b252621c24
Simeon Atanasov <sim.atanasov@gmail.com>
2025-02-15 22:38:53 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,19 +1,11 @@
-function TailoringWorkshop(countTable, LengthTable, widthTable) {
-    let areaTableCloth = countTable * (LengthTable + (2 * 0.30))*(widthTable + (2 * 0.30))
-    let kareta = countTable * (LengthTable/2)*(LengthTable/2)
-    let priceUSD = areaTableCloth * 7 + kareta * 9;
-    let priceBGN = priceUSD * 1.85;
-    console.log(`${priceUSD.toFixed(2)} USD`);
-    console.log(`${priceBGN.toFixed(2)} BGN`)
+function vacationBookList(NumberPages, Pages, Days) {
+    // 1.	Брой страници в текущата книга – цяло число в интервала [1…1000]
+    // 2.	Страници, които прочита за 1 час – цяло число в интервала [1…1000]
+    // 3.	Броят на дните, за които трябва да прочете книгата – цяло число в интервала [1…1000]
+    let Hours = (NumberPages / Pages) / Days;
+
+    // Да се отпечата на конзолата броят часове, които Жоро трябва да отделя за четене всеки ден.
+    console.log(Hours);
 }
 
-TailoringWorkshop(5, 1.00, 0.50);function TailoringWorkshop(countTable, LengthTable, widthTable) {
-    let areaTableCloth = countTable * (LengthTable + (2 * 0.30))*(widthTable + (2 * 0.30))
-    let kareta = countTable * (LengthTable/2)*(LengthTable/2)
-    let priceUSD = areaTableCloth * 7 + kareta * 9;
-    let priceBGN = priceUSD * 1.85;
-    console.log(`${priceUSD.toFixed(2)} USD`);
-    console.log(`${priceBGN.toFixed(2)} BGN`)
-}
-
-TailoringWorkshop(5, 1.00, 0.50);
+vacationBookList(212, 20, 2);
