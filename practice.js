0ec6938bf295836692ba308241a140de5bf6d979
Simeon Atanasov <sim.atanasov@gmail.com>
2025-02-16 08:18:37 +0200

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,11 +1,24 @@
-function vacationBookList(NumberPages, Pages, Days) {
-    // 1.	Брой страници в текущата книга – цяло число в интервала [1…1000]
-    // 2.	Страници, които прочита за 1 час – цяло число в интервала [1…1000]
-    // 3.	Броят на дните, за които трябва да прочете книгата – цяло число в интервала [1…1000]
-    let Hours = (NumberPages / Pages) / Days;
+function dancehall(input) {
+    let index = 0;
+    let L = Number(input[index]);
+    index++;
+    let W = Number(input[index]);
+    index++;
+    let A = Number(input[index]);
 
-    // Да се отпечата на конзолата броят часове, които Жоро трябва да отделя за четене всеки ден.
-    console.log(Hours);
+    let RoomSize = (L * 100) * (W * 100);
+    let wardrobe = A * 100 * A  * 100;
+    let bench = RoomSize / 10;
+    let freeRoom = RoomSize - wardrobe - bench;
+
+    let dancers = Math.floor(freeRoom / (40 + 7000));
+
+
+    // 1.	L – дължина на залата в метри – реално число в интервала  [10.00 … 100.00]
+    // 2.	W – ширина на залата в метри – реално число в интервала  [10.00 … 100.00]
+    // 3.	А – страна на гардероба в метри – реално число в интервала [2.00… 20.00]
+    
+    console.log(dancers);
 }
 
-vacationBookList(212, 20, 2);
+dancehall([50,25,2]);
