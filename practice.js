a22ba450f44f972c431abb3abcee3189b3dcde53
Simeon Atanasov <sim.atanasov@gmail.com>
2025-04-07 21:09:26 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,15 +1,9 @@
-function FoodDelivery(ChickenMenuCount, FishMenuCount, VegeterianMenuCount) {
-    // •	Пилешко меню –  10.35 лв. 
-    // •	Меню с риба – 12.40 лв. 
-    // •	Вегетарианско меню  – 8.15 лв. 
-    let Menus = ((ChickenMenuCount * 10.35) + (FishMenuCount * 12.40) + (VegeterianMenuCount * 8.15))
-    let Dessert = 0.2 * Menus
-    let TotalDelivery = Menus + Dessert + 2.50;
-    //     От конзолата се четат 3 реда:
-    // •	Брой пилешки менюта – цяло число в интервала [0 … 99]
-    // •	Брой менюта с риба – цяло число в интервала [0 … 99]
-    // •	Брой вегетариански менюта – цяло число в интервала [0 … 99]
-    console.log(TotalDelivery)
+function ExcellentResult(input) {
+    let index = 0;
+    let grade = Number(input[index]);
+    if (grade >=5.50) {
+        console.log('Excellent!');
+    }
 }
 
-FoodDelivery(2, 4, 3);
+ExcellentResult([5.50]);
