3f22f0ea5320cccbbe72e2ba41dfab047fce8284
Simeon Atanasov <sim.atanasov@gmail.com>
2025-04-20 09:21:31 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,11 +1,16 @@
-function passwordGuess(password) {
-    if (password == "s3cr3t!P@ssw0rd"){
-        console.log("Welcome");
-    }  else {
-        console.log("Wrong password!");
+function number100to200(input) {
+    let index = 0;
+    input = Number(input[index]);
+    if (input < 100) {
+        console.log("Less than 100");
+    } else if (input > 200) {
+        console.log("Greater than 200");
+    } else {
+        console.log("Between 100 and 200");
     }
+
 }
 
-PasswordGuess("qwerty");
-PasswordGuess("s3cr3t!P@ssw0rd");
-PasswordGuess("s3cr3t!p@ss");
+number100to200([95]);
+number100to200([120]);
+number100to200([210]);
