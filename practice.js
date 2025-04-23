7042c48646d566bb8f09ec1f23d44fd4bb7154d1
Simeon Atanasov <sim.atanasov@gmail.com>
2025-04-23 07:47:51 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,16 +1,13 @@
-function number100to200(input) {
-    let index = 0;
-    input = Number(input[index]);
-    if (input < 100) {
-        console.log("Less than 100");
-    } else if (input > 200) {
-        console.log("Greater than 200");
-    } else {
+function number100to200(number) {
+    if (number >= 100 && number <= 200) {
         console.log("Between 100 and 200");
+    } else if (number < 100)  {
+        console.log("Less than 100");
+    } else {
+        console.log("Greater than 200");
     }
-
 }
 
-number100to200([95]);
-number100to200([120]);
-number100to200([210]);
+number100to200(95);
+number100to200(120);
+number100to200(210);
