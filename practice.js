9fc85e7bf97bddf0c744b8b98102b2f052e9cb1f
Simeon Atanasov <sim.atanasov@gmail.com>
2025-04-26 15:16:05 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,13 +1,25 @@
-function number100to200(number) {
-    if (number >= 100 && number <= 200) {
-        console.log("Between 100 and 200");
-    } else if (number < 100)  {
-        console.log("Less than 100");
+function speedInformation(speed){
+    // •	При скорост до 10 (включително) отпечатайте "slow"
+    // •	При скорост над 10 и до 50 (включително) отпечатайте "average" 
+    // •	При скорост над 50 и до 150 (включително) отпечатайте "fast"
+    // •	При скорост над 150 и до 1000 (включително) отпечатайте "ultra fast" 
+    // •	При по-висока скорост отпечатайте "extremely fast"
+    speed = Number(speed);
+    if (speed <= 10) {
+        console.log("slow");
+    } else if (speed > 10 && speed <= 50) {
+        console.log("average");
+    } else if (speed > 50 && speed <= 150) {
+        console.log("fast");
+    } else if (speed > 150 && speed <= 1000) {
+        console.log("ultra fast");
     } else {
-        console.log("Greater than 200");
+        console.log("extremely fast")
     }
 }
 
-number100to200(95);
-number100to200(120);
-number100to200(210);
+speedInformation(8);
+speedInformation(49.5);
+speedInformation(126);
+speedInformation(160);
+speedInformation(3500);
